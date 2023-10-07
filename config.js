de455394f8895ea7072fd690551e87efdf0ced47
Radja Thaher <radjapolem@gmail.com>
2023-10-07 11:56:57 -0400

    init

---
+++ config.js
@@ -0,0 +1,4 @@
+module.exports = {
+    MONGODB: 'mongodb+srv://radjapolem:valkyrion123@cluster0.rlszu0p.mongodb.net/social-media-clone?retryWrites=true&w=majority',
+    SECRET_KEY: 'some very secret key'
+};
